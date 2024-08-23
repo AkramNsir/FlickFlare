4eee3d6c9e693dc1840a14af3bc4b207f4b586ae
Akram Nsir <akramnsir5@gmail.com>
2024-08-23 01:39:35 +0100

    Updated login credentials

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -10,7 +10,7 @@ export class AuthService {
   constructor(private router: Router) { }
 
   login(uname: string, pword: string) {
-    if (uname === 'akram' && pword === '1234') {
+    if (uname === 'test' && pword === '123456') {
       // return 200;
       this.isAuthenticated = true;
       if (typeof localStorage !== 'undefined') {
